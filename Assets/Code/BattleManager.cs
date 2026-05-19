5fa9226e48b16c998c62a9462c7c13ba046eff42
yaboimarf <mfolst2006@gmail.com>
2026-05-19 11:13:43 +0200

    BattleManager 0.1

---
+++ Assets/Code/BattleManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public bool isBattleActive;
+    public bool playerTurnDone;
+    public bool enemyTurnDone;
+    public GameObject battleMenu;
+    private void Update()
+    {
+        if (isBattleActive)
+        {
+            StartBattle();
+        }
+    }
+
+    public void StartBattle()
+    {
+        battleMenu.SetActive(true);
+        Debug.Log("player turn");
+        if (playerTurnDone)
+        {
+            battleMenu.SetActive(false);
+            Debug.Log("enemy turn");
+
+            if (enemyTurnDone)
+            {
+                playerTurnDone = false;
+                enemyTurnDone = false;
+            }
+        }
+    }
+    public void PlayerTurnCompleted()
+    {
+        playerTurnDone = true;
+    }
+    public void EnemyTurnCompleted()
+    {
+        if (playerTurnDone)
+        {
+            enemyTurnDone = true;
+        }
+    }
+    public void EndBattle()
+    {
+        isBattleActive = false;
+        playerTurnDone = false;
+        enemyTurnDone = false;
+        battleMenu.SetActive(false);
+    }
+}
